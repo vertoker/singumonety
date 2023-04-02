c6c84d4c635c42302ae951a462a84f7907accffb
vertoker <vertogpro@gmail.com>
2023-04-03 01:28:41 +0500

    first logic

---
+++ Singumonety/UserActions/IUserAction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singumonety.UserActions
+{
+    public interface IUserAction
+    {
+        public void Undo();
+        public void Redo();
+    }
+}
